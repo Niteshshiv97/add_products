8503c5c6e026cea14e7001cb407dc6130ebf1374
Nitesh shiv <niteshshiv71@gmail.com>
2024-12-25 14:28:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react' ;
+import Product from './component/productreact/Product.js';
+
+
+const App = () => {
+  return (
+    <>
+  <Product/>
+      
+    </>
+  )
+}
+
+export default App ;
